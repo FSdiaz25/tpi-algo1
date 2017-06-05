a3a439afc2562bee03042b064b7aff74dd29a35b
Julian Lautaro Diaz <jldiaz@ws5.labo2>
2017-06-05 20:46:25 -0300

    Resolucion parcialmente y totalmente resuelto

--- src/sudoku.cpp
+++ src/sudoku.cpp
@@ -113,14 +113,125 @@ bool sudoku_esTableroValido(Tablero t) {
 	return esTableroValido;
 }
 
+int cantEnFila(Tablero t, int f, int value){
+	int c = 0;
+	int count = 0;
+
+	while(c < 9){
+		if(t[f][c] == value)
+			count++;
+		c++;
+	}
+
+	return count;
+}
+
+bool filasOk(Tablero t){
+	int i = 0;
+	bool result = true;
+
+	while(i < 9 && result){
+		int j = 0;
+		while(j < 9 && result){
+			if(t[i][j] != 0 && cantEnFila(t, i, t[i][j]) != 1)
+				result = false;
+			j++;
+		}
+		i++;
+	}
+	return result;
+}
+
+int cantEnColumna(Tablero t, int c, int value){
+	int f = 0;
+	int count = 0;
+
+	while(f < 9){
+		if(t[f][c] == value)
+			count++;
+		f++;
+	}
+
+	return count;
+}
+
+
+bool columnasOk(Tablero t){
+	int j = 0;
+	bool result = true;
+
+	while(j < 9 && result){
+		int i = 0;
+		while(i < 9 && result){
+			if(t[i][j] != 0 && cantEnColumna(t, j, t[i][j]) != 1)
+				result = false;
+			i++;
+		}
+		j++;
+	}
+	return result;
+}
+int cantEnRegion(Tablero t, int x, int y, int value){
+	int f = 0;
+	int count = 0;
+
+	while(f < 3){
+		int c = 0;
+		while(c < 3){
+			if(t[3*x + f][3*y + c] == value)
+				count++;
+			c++;
+		}
+		f++;
+	}
+
+	return count;
+}
+bool regionValida(Tablero t, int x, int y){
+	int f = 0;
+	bool result = true;
+
+	while(f < 3 && result){
+		int c = 0;
+		while(c < 3 && result){
+			if(t[3*x + f][3*y + c] != 0 && cantEnRegion(t, x, y, t[3*x + f][3*y + c]) != 1)
+				result = false;
+			c++;
+		}
+		f++;
+	}
+	return result;
+}
+
+bool regionesOk(Tablero t){
+	int x = 0;
+	bool result = true;
+
+	while(x < 3 && result){
+		int y = 0;
+		while(y < 3 && result){
+			if(!regionValida(t, x, y))
+				result = false;
+			y++;
+		}
+		x++;
+	}
+	return result;
+}
+
 bool sudoku_esTableroParcialmenteResuelto(Tablero t) {
-	// COMPLETAR
-	return false;
+	bool res;
+
+	if(!sudoku_esTableroValido(t))
+		res = false;
+	else
+		res = filasOk(t) && columnasOk(t) && regionesOk(t);
+
+	return res;
 }
 
 bool sudoku_esTableroTotalmenteResuelto(Tablero t) {
-	// COMPLETAR
-	return false;
+	return sudoku_esTableroParcialmenteResuelto(t) && sudoku_nroDeCeldasVacias(t) == 0;
 }
 
 bool sudoku_esSubTablero(Tablero t0, Tablero t1) {
